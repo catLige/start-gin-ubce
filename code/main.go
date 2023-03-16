8f06e1fd9ee22cefe9aeaea7dd8ee60b47bbb828
catLige <127393464+catLige@users.noreply.github.com>
2023-03-16 11:07:38 +0800

    Update main.go

--- code/main.go
+++ code/main.go
@@ -13,7 +13,7 @@ func main() {
 	})
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello world",
+			"message": "hello wor",
 		})
 	})
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
